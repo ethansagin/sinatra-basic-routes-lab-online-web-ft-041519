355beb5b0c282bf42c465f648be12538dd928459
Ethan Sagin <ethansagin@gmail.com>
2019-06-03 18:51:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Ethan"
+  end
+  
+  get '/hometown' do
+    "My hometown is Cumberland"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is IDK"
+  end
+  
 end
